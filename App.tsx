8c5a4eb667893f3cbd7a8f4c4b69cd2b61c19373
arthur <arthur.rios007@gmail.com>
2024-01-23 12:04:44 -0300

    add: swipeable to history card

--- App.tsx
+++ App.tsx
@@ -20,7 +20,7 @@ export default function App() {
   }
 
   return (
-    <GestureHandlerRootView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"

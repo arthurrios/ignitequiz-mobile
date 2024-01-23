1ea5909590a100cf9b7e620d137744c0425c5c41
arthur <arthur.rios007@gmail.com>
2024-01-23 15:06:42 -0300

    add: skip card on drag

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { useEffect, useState } from 'react'
 import { Alert, Text, View } from 'react-native'
 
@@ -17,6 +19,7 @@ import Animated, {
   Easing,
   Extrapolate,
   interpolate,
+  runOnJS,
   useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
@@ -25,12 +28,16 @@ import Animated, {
 } from 'react-native-reanimated'
 import { ProgressBar } from '../../components/ProgressBar'
 import { THEME } from '../../styles/theme'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 
 interface Params {
   id: string
 }
 
 type QuizProps = (typeof QUIZ)[0]
+
+const CARD_INCLINATION = 10
+const CARD_SKIP_AREA = -200
 
 export function Quiz() {
   const [points, setPoints] = useState(0)
@@ -43,6 +50,7 @@ export function Quiz() {
 
   const shake = useSharedValue(0)
   const scrollY = useSharedValue(0)
+  const cardPosition = useSharedValue(0)
 
   const { navigate } = useNavigation()
 
@@ -164,6 +172,32 @@ export function Quiz() {
     }
   })
 
+  const onPan = Gesture.Pan()
+    .onUpdate((event) => {
+      const moveToLeft = event.translationX < 0
+
+      if (moveToLeft) {
+        cardPosition.value = event.translationX
+      }
+    })
+    .onEnd((event) => {
+      if (event.translationX < CARD_SKIP_AREA) {
+        runOnJS(handleSkipConfirm)()
+      }
+
+      cardPosition.value = withTiming(0)
+    })
+
+  const dragStyles = useAnimatedStyle(() => {
+    const rotateZ = cardPosition.value / CARD_INCLINATION
+    return {
+      transform: [
+        { translateX: cardPosition.value },
+        { rotateZ: `${rotateZ}deg` },
+      ],
+    }
+  })
+
   useEffect(() => {
     const quizSelected = QUIZ.filter((item) => item.id === id)[0]
     setQuiz(quizSelected)
@@ -204,14 +238,16 @@ export function Quiz() {
             totalOfQuestions={quiz.questions.length}
           />
         </Animated.View>
-        <Animated.View style={shakeStyleAnimated}>
-          <Question
-            key={quiz.questions[currentQuestion].title}
-            question={quiz.questions[currentQuestion]}
-            alternativeSelected={alternativeSelected}
-            setAlternativeSelected={setAlternativeSelected}
-          />
-        </Animated.View>
+        <GestureDetector gesture={onPan}>
+          <Animated.View style={[shakeStyleAnimated, dragStyles]}>
+            <Question
+              key={quiz.questions[currentQuestion].title}
+              question={quiz.questions[currentQuestion]}
+              alternativeSelected={alternativeSelected}
+              setAlternativeSelected={setAlternativeSelected}
+            />
+          </Animated.View>
+        </GestureDetector>
 
         <View style={styles.footer}>
           <OutlineButton title="Parar" onPress={handleStop} />

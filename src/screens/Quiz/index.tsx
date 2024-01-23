fcdcbe8625c7ed76936948633ab45856a70225bc
arthur <arthur.rios007@gmail.com>
2024-01-23 20:34:51 -0300

    add: sound feedback

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -30,6 +30,7 @@ import { ProgressBar } from '../../components/ProgressBar'
 import { THEME } from '../../styles/theme'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 import { OverlayFeedback } from '../../components/OverlayFeedback'
+import { Audio } from 'expo-av'
 
 interface Params {
   id: string
@@ -59,6 +60,16 @@ export function Quiz() {
 
   const route = useRoute()
   const { id } = route.params as Params
+
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect
+      ? require('../../assets/correct.mp3')
+      : require('../../assets/wrong.mp3')
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true })
+
+    await sound.setPositionAsync(0)
+    await sound.playAsync()
+  }
 
   function handleSkipConfirm() {
     Alert.alert('Pular', 'Deseja realmente pular a questão?', [
@@ -96,10 +107,12 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
-      setStatusReply(1)
       setPoints((prevState) => prevState + 1)
+      await playSound(true)
+      setStatusReply(1)
       handleNextQuestion()
     } else {
+      await playSound(false)
       setStatusReply(2)
       shakeAnimation()
     }

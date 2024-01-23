87a43d57923724fd37fc3d16c37174f6b0eee30b
arthur <arthur.rios007@gmail.com>
2024-01-23 19:15:33 -0300

    add: stars component with trophy

---
+++ src/components/Stars/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  canvas: {
+    height: 249,
+    width: 257,
+    position: 'absolute',
+    zIndex: 1,
+  },
+})
